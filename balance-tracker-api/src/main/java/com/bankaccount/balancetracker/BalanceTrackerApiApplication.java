5a055fef7433ed963c6afcf71d5c8a6baa0bd880
Thiruvenkadam Nachimuthu <venkadamktv@gmail.com>
2025-09-17 15:54:17 +0100

    Initial Spring Boot skeleten for the balance-tracker-api

---
+++ balance-tracker-api/src/main/java/com/bankaccount/balancetracker/BalanceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.bankaccount.balancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceTrackerApiApplication.class, args);
+	}
+
+}
---
+++ balance-tracker-api/src/test/java/com/bankaccount/balancetracker/BalanceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankaccount.balancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
